248913f28c6a8aaef1e4eed30f9289e4a2de8244
rashedsabbir <rashedulhasan1996@gmail.com>
2023-03-20 22:35:51 +0600

    daisyui added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ function App() {
   return (
     <div className="App">
       <h1 class="text-3xl font-bold underline">
+      <button class="btn">Button</button>
+<button class="btn btn-primary">Button</button>
+<button class="btn btn-secondary">Button</button>
+<button class="btn btn-accent">Button</button>
+<button class="btn btn-ghost">Button</button>
+<button class="btn btn-link">Button</button>
     Hello world!
   </h1>
     </div>
